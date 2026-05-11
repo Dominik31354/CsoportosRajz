7e6236a5cc729df39319e05d4eaa7033612a6140
Dominik31354 <macivagyokhehe@gmail.com>
2026-05-11 08:40:25 +0200

    LampaOszlop

--- KozosRajz.py
+++ KozosRajz.py
@@ -1,2 +1,29 @@
-#lámpa oszlop körvonala-Dominik
-#lámpa oszlop körei és színei-Barna
+import turtle
+
+hossz=50
+
+turtle.fillcolor("grey")
+turtle.begin_fill()
+i=0
+while i<2:
+    turtle.forward(hossz)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    i+=1
+turtle.end_fill()
+
+turtle.forward(-hossz)
+
+turtle.fillcolor("black")
+turtle.begin_fill()
+
+z=0
+while z<2:
+    turtle.forward(3*hossz)
+    turtle.left(90)
+    turtle.forward(300)
+    turtle.left(90)
+    z+=1
+turtle.end_fill()
+turtle.done()
